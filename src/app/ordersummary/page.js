f18c56c879889be6dc128e85ff214fe4595b3ea5
AnkitRanyal <ankitranyal0000@gmail.com>
2024-01-30 01:52:13 +0530

    Frontend code

--- src/app/ordersummary/page.js
+++ src/app/ordersummary/page.js
@@ -5,11 +5,14 @@ import Nav from "../nav"
 import Link from "next/link"
 import { redirect, useRouter } from "next/navigation"
 import Footer from "../../../fotter"
+import { useEffect, useState } from "react"
 export default function ordersummary() {
-  const currentOrder = JSON.parse(localStorage.getItem("order"))
+  let currentOrder = JSON.parse(localStorage.getItem("order"))
   const router = useRouter()
+  let [order,setorder] = useState(currentOrder)
   const loginstatus = JSON.parse(localStorage.getItem("userlogin"))
   const userlogin = useSelector(selectUserLogin);
+
 
 
   const payment = currentOrder !== null || undefined && currentOrder.length && currentOrder.map((it) => {
@@ -22,7 +25,8 @@ export default function ordersummary() {
     return it.id
 
   })
-
+  
+ 
   const orderdetails = { payment, orderid }
 
   return (<>
@@ -39,7 +43,7 @@ export default function ordersummary() {
                   <img src={item.thumbnail} className="orderimg"></img>
                   <div className="orderdetails">
                     <h3>{item.items}</h3>
-                    <h3>Subtotal :&#8377; {itemm.totalAmount}</h3><br></br>
+                    <br></br>
                     <h3>paymentMethod :{itemm.paymentMethod}</h3><br></br>
                     <h3>category : {item.category}</h3><br></br>
                     <h3>quantity : {itemm.totalItems}</h3>
@@ -51,8 +55,9 @@ export default function ordersummary() {
           ))}
         </div>
       ))
-        : null}
+      : null}
 
+      <h3 className="subtotal">Subtotal : &#8377;{currentOrder && currentOrder!=null && currentOrder.length ==1 ? currentOrder.reduce((total, i) => i.totalAmount + total, 0):currentOrder && currentOrder !== null && currentOrder.length > 1 ? currentOrder.reduce((total, i) => i.totalAmount):null}</h3>
       
       {currentOrder && currentOrder !== null && currentOrder.length && currentOrder.length == 1 ? currentOrder[0].paymentMethod === "card" && currentOrder.reduce((total, i) => i.totalAmount + total, 0) ? <Link href={`http://localhost:5000/payment/?amount=${currentOrder.reduce((total, i) => i.totalAmount + total, 0)}`} className="btn btn-success buybtn" >Pay  &#8377;{currentOrder.reduce((total, i) => i.totalAmount + total, 0)}</Link> : <Link href={"/orderplaced"} className="btn btn-success buybtn" >Pay &#8377;{currentOrder !== null && currentOrder.reduce((total, i) => i.totalAmount + total, 0)}</Link> : currentOrder && currentOrder.length > 1 ? currentOrder !== null && currentOrder[0].paymentMethod === "card" && currentOrder.reduce((total, i) => i.totalAmount) ? <Link href={`http://localhost:5000/payment/?amount=${currentOrder.reduce((total, i) => i.totalAmount)}`} className="btn btn-success buybtn" >Pay  &#8377;{currentOrder.reduce((total, i) => i.totalAmount)}</Link> : <Link href={"/orderplaced"} className="btn btn-success buybtn" >Pay &#8377;{currentOrder !== null && currentOrder.reduce((total, i) => i.totalAmount)}</Link> : null}
       {currentOrder !== null ? <div className="screenfo"><Footer></Footer></div> : <div className="summaryfooter"><Footer></Footer></div>}
